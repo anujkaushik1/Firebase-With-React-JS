b3edb3c4896eaf4a5a268b8bd762b91d79336805
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-10-25 14:31:51 +0530

    Storing Images in Firebase Storae

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Firebaseauth from './Components/Firebaseauth';
 import Firestore from './Components/Firestore';
+import Firestorage from './Components/Firestorage';
 
 
 function App() {
   return (
     <>
-      {/* <Firebaseauth/> */}
-      <Firestore/>
+      <Firebaseauth/>
+      {/* <Firestore/> */}
+      <Firestorage/>
     </>
   );
 }
